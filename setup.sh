f2a6466de34b3f50043f4f38a93d2b16da541530
Debora Duarte <deboraaline@gmail.com>
2016-02-28 19:09:30 -0300

    Installs and configures git

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
 sudo apt-get install sublime-text
+
+sudo apt-get install git
+git config --global alias.st status
+git config --global user.name "Debora Duarte"
+git config --global user.email deboraaline@gmail.com
+git config --global core.editor subl
