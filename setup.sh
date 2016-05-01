d9a7067c92bf98dfe6bea009d39c7b78ff0d8341
Debora Duarte <deboraaline@gmail.com>
2016-05-01 18:09:33 -0300

    Adds rvm and ruby installation configuration

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
+# Install sublime
 sudo apt-get install sublime-text
 
-sudo apt-get install git
+# Config git
 git config --global alias.st status
 git config --global user.name "Debora Duarte"
 git config --global user.email deboraaline@gmail.com
 git config --global core.editor subl
 
+# Install lein
 sudo mv lein /usr/bin/lein
 sudo chmod a+x /usr/bin/lein
 lein
+
+# Install rvm, ruby, etc
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+sudo apt-get install curl
+\curl -sSL https://get.rvm.io | bash -s stable
+source /home/debora/.rvm/scripts/rvm
+rvm install ruby
