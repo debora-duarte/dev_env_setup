8be8a4c9fce4bec83c78141642ec2a4cb65ecfb5
Debora Duarte <debora.duarte@nubank.com.br>
2017-03-14 16:50:10 -0300

    Alters script to install flow-bin and n

--- setup.sh
+++ setup.sh
@@ -34,10 +34,12 @@ echo "unalias cp" >> ~/.bashrc
 echo "cp ~/.config/terminator/config ~/dev/dev_env_setup/terminator-config" >> ~/.bashrc
 echo "cp ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings ~/dev/dev_env_setup/sublime-config" >> ~/.bashrc
 
-# Install nvm
+# Install nvm and node tools
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 nvm install v4.3.2 # node version fow aws lambda sdk
 echo "nvm use --delete-prefix v4.3.2 --silent" >> ~/.bashrc
+sudo npm install -g n
+sudo npm install -g flow-bin
 
 # Install stack
 curl -sSL https://get.haskellstack.org/ | sh
